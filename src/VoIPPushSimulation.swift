ced1657b3b067cd7605b5a20272a6b17dca1dba8
Михаил Капелько <kornerr@gmail.com>
2022-12-06 14:44:26 +0300

    d

---
+++ src/VoIPPushSimulation.swift
@@ -0,0 +1,19 @@
+/**/import UIKit
+
+protocol VoIPPushSimulationDelegate {
+  func voipPushSimulationDidReceivePayload(_ payload: String)
+}
+
+class VoIPPushSimulation {
+  var delegate: VoIPPushSimulationDelegate?
+
+  func simulate(payload: String, after delay: DispatchTimeInterval) {
+    let bgId = UIApplication.shared.beginBackgroundTask(expirationHandler: nil)
+    // взял прямо с Kodeco такой ужасный пример обращения к UIApplication:
+    // https://www.kodeco.com/1276414-callkit-tutorial-for-ios
+    DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+      self?.delegate?.voipPushSimulationDidReceivePayload(payload)
+      UIApplication.shared.endBackgroundTask(bgId)
+    }
+  }
+}
