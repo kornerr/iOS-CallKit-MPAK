1cfa077de03ba7becb53c2f3356ec5c8e08d1c16
Михаил Капелько <kornerr@gmail.com>
2023-02-03 07:49:46 +0300

    d

--- src/Controller.swift
+++ src/Controller.swift
@@ -3,38 +3,30 @@ import MPAK
 
 class Controller: MPAK.Controller<Model> {
   init() {
-    super.init(
-      Model(),
-      debugClassName: "Controller",
-      debugLog: { print($0) }
-    )
+    super.init(Model())
   }
 }
 
 extension Controller {
   func setupCall(_ myVC: MyViewController) {
     pipe(
-      dbg: "makeUC",
       myVC.makeUICall.eraseToAnyPublisher(),
       { $0.isCallButtonPressed = true },
       { $0.isCallButtonPressed = false }
     )
 
     pipe(
-      dbg: "makeVC",
       myVC.makeVoIPCall.eraseToAnyPublisher(),
       { $0.isCallKitOKButtonPressed = true },
       { $0.isCallKitOKButtonPressed = false }
     )
 
     pipeValue(
-      dbg: "textCI",
       myVC.textCallId.eraseToAnyPublisher(),
       { $0.textCallId = $1 }
     )
 
     pipeValue(
-      dbg: "voipCI",
       myVC.voipPushCallId.eraseToAnyPublisher(),
       { $0.voipCallId = $1 }
     )
